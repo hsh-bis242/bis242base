62ca5014cc7840bc41a47f541d2c140dcdfb74f7
Timo Cirkel <timocirkel@hotmail.com>
2024-12-11 22:38:06 +0100

    Uses Seed-Metadata to partition by the unique_key

--- willibald_dwh/macros/psa_view.sql
+++ willibald_dwh/macros/psa_view.sql
@@ -1,10 +1,22 @@
-{% macro psa_view(p_psa_table, p_unique_key) %}
+
+{% macro psa_view(p_psa_table) %}
+
+{% if execute %}
+
+  {% set v_psa_table_node = graph.nodes.values()
+     | selectattr("resource_type", "equalto", "seed")
+     | selectattr("name", "equalto", p_psa_table)
+     | first %}
+
+{% endif %}
+  
+
 SELECT
     sys_loadingid,
-    {{ debug() }}
-    LEAD(sys_loadingid) OVER (PARTITION BY {{ graph.nodes[p_psa_table].meta.unique_key }} ORDER BY sys_loadingid ASC) IS NULL AS sys_islatest,
+    {% if execute %}LEAD(sys_loadingid) OVER (PARTITION BY {{ v_psa_table_node.meta.unique_key }} ORDER BY sys_loadingid ASC) IS NULL AS sys_islatest,{% endif %}
     sys_cdc,
     sys_checksum,
 	{{ dbt_utils.star(from=ref(p_psa_table), except=['sys_loadingid','sys_cdc','sys_checksum']) }}
 FROM {{ ref(p_psa_table) }}
+
 {% endmacro %}
