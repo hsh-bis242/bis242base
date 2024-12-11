b7d50852b6a7a6659c191de953615129971052d0
Timo Cirkel <timocirkel@hotmail.com>
2024-12-11 14:39:00 +0100

    Introduces seed metadata. Breaks the project

--- willibald_dwh/macros/psa_view.sql
+++ willibald_dwh/macros/psa_view.sql
@@ -1,7 +1,8 @@
 {% macro psa_view(p_psa_table, p_unique_key) %}
 SELECT
     sys_loadingid,
-    LEAD(sys_loadingid) OVER (PARTITION BY {{p_unique_key}} ORDER BY sys_loadingid ASC) IS NULL AS sys_islatest,
+    {{ debug() }}
+    LEAD(sys_loadingid) OVER (PARTITION BY {{ graph.nodes[p_psa_table].meta.unique_key }} ORDER BY sys_loadingid ASC) IS NULL AS sys_islatest,
     sys_cdc,
     sys_checksum,
 	{{ dbt_utils.star(from=ref(p_psa_table), except=['sys_loadingid','sys_cdc','sys_checksum']) }}
