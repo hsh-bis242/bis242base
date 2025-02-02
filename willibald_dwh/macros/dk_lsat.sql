6d3cedd6c216c3d2b8a4fce506b9ad7bb58d44b9
Timo Cirkel <timocirkel@hotmail.com>
2025-02-02 14:12:15 +0100

    adds a working query for reference history
    Signed-off-by: Timo Cirkel <timocirkel@hotmail.com>

--- willibald_dwh/macros/dk_lsat.sql
+++ willibald_dwh/macros/dk_lsat.sql
@@ -1,17 +1,20 @@
 {% macro dk_lsat    (source_model_name, driving_keys, follower_keys) -%}
 
 -- Needs to be something like this:
-/* WITH cte_lk_history AS (
-	 SELECT	v_webshop_produktkategorie.katid,
-			v_webshop_produktkategorie.oberkatid,
-			v_webshop_produktkategorie.sys_loadingid
-	   FROM	dev_willibald_dwh.willibald_psa_sl.v_webshop_produktkategorie
-	QUALIFY	COALESCE(LAG(v_webshop_produktkategorie.oberkatid) OVER (PARTITION BY v_webshop_produktkategorie.katid ORDER BY v_webshop_produktkategorie.sys_loadingid), '') <> v_webshop_produktkategorie.oberkatid
+/*
+WITH cte_reference_history AS (
+	SELECT	sys_loadingid,
+			sys_cdc,
+			katid,
+			oberkatid,
+			LEAD(sys_loadingid) OVER (PARTITION BY katid ORDER BY sys_loadingid) AS sys_loadingid_validto
+	  FROM 	willibald_psa_sl.v_webshop_produktkategorie
 )
-SELECT	md5(cast(coalesce(cast(cte_lk_history.katid as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(cte_lk_history.oberkatid as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) AS hkey_lnk_productcategory_supercategory,
-		cte_lk_history.sys_loadingid AS sys_validfrom_loadingid,
-		LEAD(cte_lk_history.sys_loadingid) OVER (PARTITION BY cte_lk_history.katid ORDER BY cte_lk_history.sys_loadingid) AS sys_validto_loadingid
- FROM 	cte_lk_history;
+SELECT	md5(cast(coalesce(cast(cte_reference_history.katid as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(cte_reference_history.oberkatid as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) AS hkey_lnk_productcategory_supercategory,
+		sys_loadingid,
+		sys_loadingid_validto
+  FROM 	cte_reference_history
+ WHERE 	sys_cdc != 'D';
 */
 -- But this is not working properly.
 
