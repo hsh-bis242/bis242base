76c177e98b2b09f7d7715d2b92aee4573310fb4e
Timo Cirkel <timocirkel@hotmail.com>
2025-03-02 11:34:11 +0100

    Adds a link between customers and credit cards that is loaded from roadshow sales

---
+++ willibald_dwh/models/il_rdv/lnk_roadshowcustomer_creditcard.sql
@@ -0,0 +1,30 @@
+{%- set yaml_metadata -%}
+
+source_model_name: v_roadshow_bestellung
+hubs:
+  - name: hub_customer
+    bk_columns:
+      - column_name: kundeid
+        business_name: customer_id
+  - name: hub_creditcard
+    bk_columns:
+      - column_name: kreditkarte
+        business_name: creditcard_number
+      - column_name: kkfirma
+        business_name: issuer
+      - column_name: gueltigbis
+        business_name: validto
+        
+transactional_attributes: []
+    
+{%- endset -%}
+
+{%- set metadata_dict = fromyaml(yaml_metadata) -%}
+
+{{
+    link(
+        source_model_name =         metadata_dict.get('source_model_name'),
+        hubs =                      metadata_dict.get('hubs'),
+        transactional_attributes =  metadata_dict.get('transactional_attributes')
+    )
+}}
