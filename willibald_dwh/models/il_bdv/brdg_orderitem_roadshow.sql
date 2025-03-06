a777b9b50e230f8681e97f0c8873e7414261c341
Timo Cirkel <timocirkel@hotmail.com>
2025-03-06 13:01:44 +0100

    Adds sys_loadingid_validto column to sattelites. Fixes roadshow-orderitem-bridge

--- willibald_dwh/models/il_bdv/brdg_orderitem_roadshow.sql
+++ willibald_dwh/models/il_bdv/brdg_orderitem_roadshow.sql
@@ -10,7 +10,7 @@ SELECT  lnk_cust_cc.hkey_hub_creditcard,
     ON  dklsat_cust_cc.hkey_lnk_customer_creditcard = lnk_cust_cc.hkey_lnk_customer_creditcard
 )
 SELECT      lh.loadingts AS sys_loadingts,
-            sat_rs_context.sys_loadingid,
+            lh.loadingid AS sys_loadingid,
             sat_rs_context.hkey_lnk_roadshowsale,
             NULL AS hkey_hub_webshoporderitem,
     		    lnk_rs.roadshowsale_id,
@@ -18,7 +18,7 @@ SELECT      lh.loadingts AS sys_loadingts,
             COALESCE(hub_cc_cust.customer_id, hub_cust.customer_id) AS customer_id
   FROM		  {{ ref("loading_history") }} lh
   JOIN      {{ ref("lsat_roadshowsale_context") }} sat_rs_context
-    ON      lh.loadingid >= sat_rs_context.sys_loadingid
+    ON      lh.loadingid <= sat_rs_context.sys_loadingid
   JOIN      {{ ref("lnk_roadshowsale") }}	lnk_rs
     ON      lnk_rs.hkey_lnk_roadshowsale = sat_rs_context.hkey_lnk_roadshowsale
   LEFT JOIN	cte_lnk_cust_cc lnk_cust_cc
@@ -29,3 +29,6 @@ SELECT      lh.loadingts AS sys_loadingts,
     ON		  hub_cc_cust.hkey_hub_customer  = lnk_cust_cc.hkey_hub_customer
   LEFT JOIN	{{ ref("hub_customer") }} hub_cust
     ON		  hub_cust.hkey_hub_customer = lnk_rs.hkey_hub_customer
+  JOIN      {{ ref("hub_product") }} hub_prdct
+    ON      hub_prdct.hkey_hub_product = lnk_rs.hkey_hub_product
+  JOIN      {{ ref("hub_roadshow") }} hub_rs  
