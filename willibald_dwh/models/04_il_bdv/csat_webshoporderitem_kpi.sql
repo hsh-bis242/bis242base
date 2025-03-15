8dd1ed8cf68c986d297cb7d4dd44ea1e054a07f4
Timo Cirkel <timocirkel@hotmail.com>
2025-03-15 21:17:24 +0100

    Modifies DIM_DATE

--- willibald_dwh/models/04_il_bdv/csat_webshoporderitem_kpi.sql
+++ willibald_dwh/models/04_il_bdv/csat_webshoporderitem_kpi.sql
@@ -10,7 +10,7 @@ SELECT  brdg.hkey_hub_webshoporderitem,
         sat_wso.requesteddate,
         IFNULL(lnk_wsoi_dlvry.deliverydate, DATE '9999-12-31') AS deliverydate,
         DATE_DIFF('day', lnk_wsoi_dlvry.deliverydate, sat_wso.requesteddate) AS deviation_delivery_request_days,
-        fsc.federalstate_code
+        rpf.federal_state_key as federalstate_key
   FROM  {{ ref("brdg_orderitem_webshop") }} brdg
   JOIN  {{ effective_sat("sat_webshoporderitem_context", "sat_wsoi", "hkey_hub_webshoporderitem", "brdg") }}
   JOIN  {{ effective_sat("sat_webshoporder_context", "sat_wso", "hkey_hub_webshoporder", "brdg") }}
@@ -19,5 +19,3 @@ SELECT  brdg.hkey_hub_webshoporderitem,
   JOIN  {{ effective_sat("sat_deliveryaddress_context", "sat_dlvry", "hkey_hub_deliveryaddress", "brdg") }}
   JOIN  {{ ref("ref_postalcode_federalstate") }} rpf
     ON  rpf.postalcode = sat_dlvry.postalcode
-  JOIN  {{ ref("federalstatecode") }} fsc
-    ON  fsc.key = rpf.federal_state_key
