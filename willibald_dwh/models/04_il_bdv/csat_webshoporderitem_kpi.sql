c5f51a90b5ed816cf2b0b40594e88ba8b3dc241c
Timo Cirkel <timocirkel@hotmail.com>
2025-03-23 20:09:56 +0100

    Resolves #63

--- willibald_dwh/models/04_il_bdv/csat_webshoporderitem_kpi.sql
+++ willibald_dwh/models/04_il_bdv/csat_webshoporderitem_kpi.sql
@@ -1,6 +1,6 @@
 SELECT  brdg.hkey_hub_webshoporderitem,
         brdg.sys_loadingid,
-        sat_wsoi.sys_loadingid_validto,
+        LEAD(brdg.sys_loadingid, 1, 2^31 - 1) OVER (PARTITION BY {{ brdg.hkey_hub_webshoporderitem }} ORDER BY brdg.sys_loadingid) AS sys_loadingid_validto,
         sat_wsoi.quantity,
         sat_wsoi.amount,
         sat_wso.discount,
